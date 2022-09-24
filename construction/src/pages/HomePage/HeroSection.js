f5b72d378af3ca863db5594dc45e871d1c767895
Max-Ranholi <ranholimax@gmail.com>
2022-09-23 21:12:46 -0300

    assets

--- construction/src/pages/HomePage/HeroSection.js
+++ construction/src/pages/HomePage/HeroSection.js
@@ -1,7 +1,23 @@
+import * as S from "./HsStyled";
+import * as I from "./ImagesStyled";
+import * as T from "./TextsStyled";
+import logo from "../Assets/MULHER-MAIN-SECTION.png"
+
+
 const HeroSection = () => {
     return (
         <div>
-            <h1>HeroSection</h1>
+            <S.Container>
+                <T.ContainerTexts>
+                <h1>GECOM</h1>
+                <h3>Tudo em gesso</h3>
+                <p>Material, Ferramentas e Equipamentos</p>
+                <T.button>Pedero</T.button>
+                </T.ContainerTexts>
+                <I.ContainerImages>
+                <I.Image src={logo} alt="mulher segurando dois capacetes"/>
+                </I.ContainerImages>
+            </S.Container>
         </div>
     )
 }
