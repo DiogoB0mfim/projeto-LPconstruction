df5f39d3267de4349f839d7c53ebed06af2b39c7
Max-Ranholi <ranholimax@gmail.com>
2022-09-23 22:01:08 -0300

    home-page

--- construction/src/pages/HomePage/ImagesStyled.js
+++ construction/src/pages/HomePage/ImagesStyled.js
@@ -2,13 +2,22 @@ import styled from "styled-components";
 
 export const ContainerImages = styled.div`
 display: flex;
-height: 90vh;
 width: 50%;
 align-items: flex-end;
 justify-content: end;
-
 `
 export const Image = styled.img`
 background-image: url(${props => props.img});
-height: 50rem;
+width: 100%;
+@media (max-width: 768px) {
+    display: none;
+}
+`
+export const ImageMobile = styled.img`
+background-image: url(${props => props.img});
+display: none;
+border: 1px black solid;
+@media (max-width: 768px) {
+   
+}
 `
--- construction/src/pages/HomePage/TextsStyled.js
+++ construction/src/pages/HomePage/TextsStyled.js
@@ -1,27 +1,44 @@
 import styled from "styled-components";
 
 export const ContainerTexts = styled.div`
-height: 90%;
+box-sizing: border-box;
+height: 100vh;
 width: 50%;
 padding-left: 5rem;
 line-height: 1rem;
 padding-top: 12rem;
 
+
+@media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    padding-left: 1rem;
+    line-height: 2rem;
+    padding-top: 6rem;
+    width: 100vw;
+    height: 100vh;
+}
+
 h1 {
     color: white;
     font-size: 12rem;
+    @media (max-width: 768px) {
+        font-size: 5.5rem;
+}
 }
 h3 {
     color: white;
     font-size: 3rem;
+    @media (max-width: 768px) {
+        font-size: 1.5rem;
+}
 }
 p {
     color: white;
     font-size: 2rem;
+    @media (max-width: 768px) {
+        font-size: 1rem;
 }
-
-@media (max-width: 768px) {
-    
 }
 `
 
@@ -33,4 +50,12 @@ background-color: #4F04E1;
 border: none;
 color: white;
 font-size: 3rem;
+
+@media (max-width: 768px) {
+margin-left: -0.1rem;
+height: 3rem;
+width: 12rem;
+background-color: #4F04E1;
+font-size: 2.2rem;
+}
 `
