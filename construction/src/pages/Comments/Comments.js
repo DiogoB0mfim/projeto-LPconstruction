96c2ede12e7cb4c749eeeedaca4ca42b9f32e0c2
DiogoB0mfim <diogohbomfim@gmail.com>
2022-09-27 20:09:49 -0300

    section de comentários finalizada

---
+++ construction/src/pages/Comments/Comments.js
@@ -0,0 +1,38 @@
+import * as S from "./CommentsStyled"
+import pessoa1 from "../../Assets/pessoa-comments01.png"
+import pessoa2 from "../../Assets/pessoa-comments02.png"
+import estrelas from "../../Assets/5stars.png"
+
+const Comments = () => {
+    return (
+        
+            <S.CommentsDiv>
+                <S.CommentsTitle>Comentários</S.CommentsTitle>
+                <S.CommentsContainer>
+                    <S.CommentsCard>
+                        <img src={pessoa1} alt="imagem pessoa"/>
+                        <S.CommentsCardH3>Maria</S.CommentsCardH3>
+                        <S.CommentsCardP>Adoro o serviço da pederos, são muito bons com gesso haha.</S.CommentsCardP>
+                        <img src={estrelas} alt="5 estrelas"/>
+                    </S.CommentsCard>
+
+                    <S.CommentsCard>
+                        <img src={pessoa2} alt="imagem pessoa"/>
+                        <S.CommentsCardH3>Jorge</S.CommentsCardH3>
+                        <S.CommentsCardP>Adoro o serviço da pederos, são muito bons com gesso haha.</S.CommentsCardP>
+                        <img src={estrelas} alt="5 estrelas"/>
+                    </S.CommentsCard>
+
+                    <S.CommentsCard>
+                        <img src={pessoa2} alt="imagem pessoa"/>
+                        <S.CommentsCardH3>Carlos</S.CommentsCardH3>
+                        <S.CommentsCardP>Adoro o serviço da pederos, são muito bons com gesso haha.</S.CommentsCardP>
+                        <img src={estrelas} alt="5 estrelas"/>
+                    </S.CommentsCard>
+                </S.CommentsContainer>
+            </S.CommentsDiv>
+        
+    )
+}
+
+export default Comments;
---
+++ construction/src/pages/Comments/CommentsStyled.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const CommentsDiv = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const CommentsContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  border-radius: 15px;
+  gap: 1rem;
+  width: 85%;
+  padding: 40px;
+  background-image: linear-gradient(#4f04e1, #a787e4);
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    width: 90%;
+    padding: 0rem;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+  }
+
+  @media (max-width: 1366px) {
+    width: 90%;
+    padding: 1rem;
+  }
+`;
+
+export const CommentsCard = styled.div`
+  background-color: #ffff;
+  border-radius: 15px;
+  padding: 15px;
+  width: 30%;
+  height: 100%;
+  line-height: 1.8rem;
+
+  @media (max-width: 768px) {
+    width: 95%;
+  }
+`;
+
+export const CommentsCardP = styled.p`
+  color: #3d3d3d;
+  font-size: 1.25rem;
+
+  @media (max-width: 1366px) {
+    font-size: 1rem;
+  }
+`;
+
+export const CommentsCardH3 = styled.h3`
+  @media (max-width: 1366px) {
+    font-size: 1.6rem;
+  }
+`;
+
+export const CommentsTitle = styled.h1`
+  color: #4f04e1;
+`;
